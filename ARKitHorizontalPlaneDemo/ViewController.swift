744c0f42942692feeacee018c8818f48f8734f7f
dannertech <danner@danner.tech>
2019-09-04 05:18:59 -0400

    finished tutorial by Jayven N via AppCoda

--- ARKitHorizontalPlaneDemo/ViewController.swift
+++ ARKitHorizontalPlaneDemo/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Uncomment to configure lighting
         // configureLighting()
+        addTapGestureToSceneView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,12 +32,85 @@ class ViewController: UIViewController {
     
     func setUpSceneView() {
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
+        sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     func configureLighting() {
         sceneView.autoenablesDefaultLighting = true
         sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    @objc func addShipToSceneView(withGestureRecognizer recognizer: UIGestureRecognizer){
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        guard let hitTestResult = hitTestResults.first else { return }
+        let translation = hitTestResult.worldTransform.translation
+        let x = translation.x
+        let y = translation.y
+        let z = translation.z
+        
+        guard let shipScene = SCNScene(named: "ship.scn"),
+            let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false) else { return }
+        
+        shipNode.position = SCNVector3(x,y,z)
+        sceneView.scene.rootNode.addChildNode(shipNode)
+    }
+    
+    func addTapGestureToSceneView(){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addShipToSceneView(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+}
+
+extension ViewController : ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        let width = CGFloat(planeAnchor.extent.x)
+        
+        let height = CGFloat(planeAnchor.extent.z)
+        
+        let plane = SCNPlane(width: width, height: height)
+        
+        plane.materials.first?.diffuse.contents = UIColor.transparentLightBlue
+        
+        let planeNode = SCNNode(geometry: plane)
+        
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        
+        planeNode.position = SCNVector3(x,y,z)
+        planeNode.eulerAngles.x = -.pi/2
+        
+        node.addChildNode(planeNode)
+        
+   
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor,
+        let planeNode = node.childNodes.first,
+        let plane = planeNode.geometry as? SCNPlane
+        
+            else { return }
+        
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+        
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        
+        planeNode.position = SCNVector3(x, y, z)
     }
     
 }
